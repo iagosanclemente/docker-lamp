556fe26c5b0bdff937da2cca9ecb1b32a371904f
iagosanclemente <a24iagosr@iessanclemente.net>
2024-10-27 14:56:53 +0100

    Versión final de la tarea de la UD2

---
+++ www/UD2/entregaTarea/footer.php
@@ -0,0 +1,10 @@
+<?php
+    function createFooter(){
+
+        return '
+        <footer class="bg-dark text-white text-center py-3"> 
+            <p> Ⓒ 2024/2025 - Iago Sánchez Rodríguez - Todos los derechos reservados. </p>
+        </footer>
+        ';
+    }
+?>
---
+++ www/UD2/entregaTarea/header.php
@@ -0,0 +1,10 @@
+<?php
+    function createHeader() {
+        return '
+        <header class="bg-primary text-white text-center py-3">
+            <h1> UD2 - Iago Sánchez </h1>
+            <p> Desarrollo de una Aplicación Básica en PHP </p>
+        </header> 
+        ';
+    }
+?>
